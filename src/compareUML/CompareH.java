4d2a28dd871f1f11745814536ffd40ca4e7be87f
ShupengHu <hushupenguk@gmail.com>
2015-02-17 15:32:24 +0000

    modify compareH

--- src/compareUML/CompareH.java
+++ src/compareUML/CompareH.java
@@ -13,7 +13,7 @@ public class CompareH {
 	double umlelenumber = 0;
 	double assoelenumber = 0;
 
-	public CompareH() {		
+	public CompareH() {
 		LecturerExample l = new LecturerExample();
 		StudentExample s = new StudentExample();
 		ElementsPreprocessor lectureProcessor = new ElementsPreprocessor(
@@ -26,80 +26,83 @@ public class CompareH {
 				.preprocessForUseCase();
 
 		// ---------------------------------------------------------------------------------------
-		String[] StudentKeys = forkeys(StudentMap);		// warning
+		String[] StudentKeys = forkeys(StudentMap); // warning
 		String[] LecturerKeys = forkeys(LecturerMap);
-        //----------------------------------------------------------------------------------------
-		
-		
-		
+		// ----------------------------------------------------------------------------------------
+
 		for (String key : LecturerKeys) {
 			ArrayList<PackagedElement> lecturerElements = LecturerMap.get(key);
 			ArrayList<PackagedElement> studentElements = StudentMap.get(key);
-			for (PackagedElement packagedElement : lecturerElements) {             
+			for (PackagedElement packagedElement : lecturerElements) {
 				// lecturer's packagedElement
-				GeneralizableElement umlele = (GeneralizableElement) packagedElement; 
-					// pick student's packagedElement
-					for (PackagedElement stdumlEle : studentElements) { 
+				GeneralizableElement geneele = (GeneralizableElement) packagedElement;
+				// pick student's packagedElement
+				for (PackagedElement stdgeneEle : studentElements) {
+					GeneralizableElement stdgeneele = (GeneralizableElement) stdgeneEle;
+					if (geneele.isGeneralizable() == true
+							&& stdgeneele.isGeneralizable() == true) {
 						// pick student's GeneralizableElement
-						if (umlele.getName().compareToIgnoreCase( 
-								((GeneralizableElement) stdumlEle).getName()) == 0) {
+						if (geneele.getName().compareToIgnoreCase(
+								(stdgeneele).getName()) == 0) {
 							umlelenumber++;
 						}
-						// pick student's AssociationElement
-						else if (packagedElement instanceof AssociationElement) { 
-							AssociationElement assoele = (AssociationElement) packagedElement;
-							for (PackagedElement stdassoele : studentElements) {
-								if (assoele.getFirstMemberEnd().compareToIgnoreCase(
-										((AssociationElement) stdassoele)
-												.getFirstMemberEnd()) == 0
-										&& assoele
-												.getSecondMemberEnd()
-												.compareToIgnoreCase(
-														((AssociationElement) stdassoele)
-																.getSecondMemberEnd()) != 0) {
-									assoelenumber = assoelenumber+0.5;
-								} else if (assoele.getFirstMemberEnd()
-										.compareToIgnoreCase(
-												((AssociationElement) stdassoele)
-														.getFirstMemberEnd()) != 0
-										&& assoele
-												.getSecondMemberEnd()
-												.compareToIgnoreCase(
-														((AssociationElement) stdassoele)
-																.getSecondMemberEnd()) == 0) {
-									assoelenumber = assoelenumber + 0.5;
-								} else if (assoele.getFirstMemberEnd()
-										.compareToIgnoreCase(
-												((AssociationElement) stdassoele)
-														.getFirstMemberEnd()) == 0
-										&& assoele
-												.getSecondMemberEnd()
-												.compareToIgnoreCase(
-														((AssociationElement) stdassoele)
-																.getSecondMemberEnd()) == 0) {
-									assoelenumber++;
-								}
+					}
+					// pick student's AssociationElement
+					else if (packagedElement instanceof AssociationElement) {
+						AssociationElement assoele = (AssociationElement) packagedElement;
+						for (PackagedElement stdassoele : studentElements) {
+							if (assoele.getFirstMemberEnd()
+									.compareToIgnoreCase(
+											((AssociationElement) stdassoele)
+													.getFirstMemberEnd()) == 0
+									&& assoele
+											.getSecondMemberEnd()
+											.compareToIgnoreCase(
+													((AssociationElement) stdassoele)
+															.getSecondMemberEnd()) != 0) {
+								assoelenumber = assoelenumber + 0.5;
+							} else if (assoele.getFirstMemberEnd()
+									.compareToIgnoreCase(
+											((AssociationElement) stdassoele)
+													.getFirstMemberEnd()) != 0
+									&& assoele
+											.getSecondMemberEnd()
+											.compareToIgnoreCase(
+													((AssociationElement) stdassoele)
+															.getSecondMemberEnd()) == 0) {
+								assoelenumber = assoelenumber + 0.5;
+							} else if (assoele.getFirstMemberEnd()
+									.compareToIgnoreCase(
+											((AssociationElement) stdassoele)
+													.getFirstMemberEnd()) == 0
+									&& assoele
+											.getSecondMemberEnd()
+											.compareToIgnoreCase(
+													((AssociationElement) stdassoele)
+															.getSecondMemberEnd()) == 0) {
+								assoelenumber++;
 							}
 						}
 					}
-				} 				
+				}
 			}
+		}
 		correctnumber = assoelenumber + umlelenumber;
 		System.out.println("correct number in total is " + correctnumber);
 		System.out.println("correct umlelement number is " + umlelenumber);
 		System.out.println("correct association number is " + assoelenumber);
-		}				
+	}
 
-//get keys from result of preprocessForUseCase
-public static String[] forkeys(HashMap<String, ArrayList<PackagedElement>> h) {
-	Set<String> myKeys = h.keySet();
-	String[] keys = new String[myKeys.size()];
-	myKeys.toArray(keys);
-	return keys;
-}
+	// get keys from result of preprocessForUseCase
+	public static String[] forkeys(HashMap<String, ArrayList<PackagedElement>> h) {
+		Set<String> myKeys = h.keySet();
+		String[] keys = new String[myKeys.size()];
+		myKeys.toArray(keys);
+		return keys;
+	}
 
-public static double correctrate(double correctnumber, double totalnumber){
-	double correctrate=correctnumber/totalnumber;
-	return correctrate;
-}
+	public static double correctrate(double correctnumber, double totalnumber) {
+		double correctrate = correctnumber / totalnumber;
+		return correctrate;
+	}
 }
