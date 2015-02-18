abf21dc31f22b5f3dcf4e4f2d430c12bd42c840c
Nicolas1991 <nicolas19911217@hotmail.com>
2015-02-18 14:47:28 +0000

    update student example

--- src/compareUML/StudentExample.java
+++ src/compareUML/StudentExample.java
@@ -11,11 +11,14 @@ public class StudentExample {
 		// TODO Auto-generated constructor stub
 		this.umlElements = new ArrayList<PackagedElement>();
 		fileName = "usecase demo";
-		this.umlElements.add(new UMLElement("Actor", "xcfghjop", "Paul"));
-		this.umlElements.add(new UMLElement("Actor", "asdfghjk", "Mike"));
-		this.umlElements.add(new UMLElement("UseCase", "werfghjm", "New usecase"));
+		this.umlElements.add(new GeneralizableElement("Actor", "xcfghjop", "Paul",null));
+		this.umlElements.add(new GeneralizableElement("Actor", "asdfghjk", "Mike",null));
+		this.umlElements.add(new UseCaseElement("UseCase", "werfghjm", "New usecase",null,null,null,null));
+		this.umlElements.add(new UseCaseElement("UseCase", "werfghjm", "New usecase",null,null,null,null));		
 		this.umlElements.add(new AssociationElement("xdfthyjklop", "xcfghjop", "werfghjm"));
 		this.umlElements.add(new AssociationElement("wertghjkop", "asdfghjk", "werfghjm"));
+		
+		
 	}
 	
 	public ArrayList<PackagedElement> getUmlElements() {
