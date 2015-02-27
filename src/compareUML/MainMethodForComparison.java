1ccc0ff931694e233a5748d3b8b79ac717a6ad6b
Xibo Wang <paulop5288@gmail.com>
2015-02-27 15:54:43 +0000

    delete class

--- src/compareUML/MainMethodForComparison.java
+++
@@ -1,9 +0,0 @@
-package compareUML;
-
-public class MainMethodForComparison {
-	public static void main(String[] args) {
-		CompareH h=new CompareH();
-		GUI frame=new GUI();
-		frame.setVisible(true);
-	}
-}
