e7466db3191c426d19bb6de4c62846bec1ccde01
Xibo Wang <paulop5288@gmail.com>
2015-04-08 23:47:01 +0100

    remove unused imports

--- src/FileHandler/TextReader.java
+++ src/FileHandler/TextReader.java
@@ -1,17 +1,10 @@
 package FileHandler;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.file.Files;
 import java.util.ArrayList;
 
-import com.sun.org.apache.regexp.internal.recompile;
 
 public abstract class TextReader {
 
