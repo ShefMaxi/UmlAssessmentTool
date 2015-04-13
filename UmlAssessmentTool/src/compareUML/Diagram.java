fba0ee289d4d6afc696bcd3cbdac3d102b07af42
Xibo Wang <paulop5288@gmail.com>
2015-04-13 14:43:24 +0100

    Add toString method for Diagram class

--- UmlAssessmentTool/src/compareUML/Diagram.java
+++ UmlAssessmentTool/src/compareUML/Diagram.java
@@ -1,6 +1,7 @@
 package compareUML;
 
 import java.util.ArrayList;
+
 import packagedElements.PackagedElement;
 
 // usecase		 : 1
@@ -22,5 +23,29 @@ public class Diagram {
 	
 	public int getDiagramType() {
 		return diagramType;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		String diagramTypeString;
+		switch (diagramType) {
+		case 1:
+			diagramTypeString = "UseCase Diagram";
+			break;
+		case 2:
+			diagramTypeString = "Class Diagram";
+			break;
+		case 3:
+			diagramTypeString = "Activity Diagram";
+			break;
+		case 4:
+			diagramTypeString = "StateMachine Diagram";
+			break;
+		default:
+			diagramTypeString = "Unrecogized diagram";
+			break;
+		}
+		return diagramTypeString + " : contains " + allElements.size() + " elements..." ;
 	}
 }
