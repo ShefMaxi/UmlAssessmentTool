52c7db8c77e00c5f6365235e74acd5fe6c47bf4f
Xibo Wang <paulop5288@gmail.com>
2015-04-21 20:02:23 +0100

    Update TextRead class with new methods
    Example of how to use is provided.

--- UmlAssessmentTool/src/fileHandler/TextReader.java
+++ UmlAssessmentTool/src/fileHandler/TextReader.java
@@ -4,29 +4,62 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
 
-public abstract class TextReader {
+// an example of how to use this class is provided in the bottom
 
-	public TextReader() {
-		// TODO Auto-generated constructor stub
+public class TextReader {
+	private Map<String, String> studentInfoMap = new HashMap<>();
+	private Map<String, String> studentFilePathMap = new HashMap<>();
+	
+	public TextReader(List<String> textFilePaths) {
+		// process all text files
+		for (String textFile : textFilePaths) {
+			parseTextFile(textFile);
+		}
 	}
 	
-	public static ArrayList<String> parseTextFile(String path) {
-		ArrayList<String> result = new ArrayList<String>();
+	// accessors
+	public Map<String, String> getStudentFilePathMap() {
+		return studentFilePathMap;
+	}
+	
+	public Map<String, String> getStudentInfoMap() {
+		return studentInfoMap;
+	}
+	
+	
+	public void parseTextFile(String path) {
 		BufferedReader textReader = null;
 		try {
 			textReader = new BufferedReader(new FileReader(path));
+			
+			String rootPath = "";
+			if (path.contains("\\")) {
+				rootPath = path.substring(0, path.lastIndexOf("\\") + 1);
+			} else {
+				rootPath = path.substring(0, path.lastIndexOf("/") + 1);
+			}
+			
 			String firstLine = textReader.readLine();
-			result.add(firstLine.substring(firstLine.indexOf("(") + 1, firstLine.indexOf(")")));	// find out the name
-			String lastLine = null;
+			// find username and student name
+			String studentUsername = firstLine.substring(firstLine.indexOf("(") + 1, firstLine.indexOf(")"));	
+			String studentName = firstLine.substring(firstLine.indexOf(" ") + 1, firstLine.indexOf("(") - 1);
+			this.studentInfoMap.put(studentUsername, studentName);
+			
+			String lastLine = "";
 			while (textReader.ready()) {
 				lastLine = textReader.readLine();
 				if (lastLine.indexOf("Filename: ") > -1) {
-					result.add(lastLine.substring(lastLine.lastIndexOf("Filename: ") + 10));
+					this.studentFilePathMap.put(studentUsername, rootPath + lastLine.substring(lastLine.indexOf(":") + 2));
 					break;
 				}
 			}
+			
 		} catch (IOException e) {
 			System.err.println("File not found.");
 		} finally {
@@ -36,15 +69,21 @@ public abstract class TextReader {
 				System.err.println("fail to close file.");
 			}
 		}
-		return result;
+		return;
 	}
 	
-	public static void main(String[] args) {
-		ArrayList<String> a = TextReader.parseTextFile("UML Exercise 1  Requirements_aca12aa_"
-			+ "attempt_2013-11-14-14-15-40.txt");
-		System.out.println(a.get(0));
-		System.out.println(a.get(1));
-
-	}
+	/*
+	 * This is an example of how to use TextReader
+	 */
+	
+//	public static void main(String[] args) {
+//		
+//		ArrayList<String> a = new ArrayList<>();
+//		a.add("C:/Users/Paul/Documents/GitHub/UmlAssessmentTool/UmlAssessmentTool/UML Exercise 1  Requirements_aca12aa_attempt_2013-11-14-14-15-40.txt");
+//		TextReader reader = new TextReader(a);
+//		reader.getStudentFilePathMap();
+//		reader.getStudentInfoMap();
+//
+//	}
 
 }
